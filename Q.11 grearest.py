f9cf219482f4788cfe2176cd12411c83c5c25720
Saurabhvema <100114223+Saurabhvema@users.noreply.github.com>
2022-03-06 17:45:06 +0530

    Create Q.11 grearest.py

---
+++ Q.11 grearest.py
@@ -0,0 +1,9 @@
+a=int(input("enter 1st no"))
+b=int(input("enter 2nd no"))
+c=int(input("enter  3rd no"))
+if(a>b and a>c):
+   print("a is grearest")
+elif(b>a and b>c):
+   print("b is grearest")
+else:
+   print("c is greatest")
